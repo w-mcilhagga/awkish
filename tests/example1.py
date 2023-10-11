59fc24fc7161f6ac90c33eb56ab13b1915270c96
w-mcilhagga <william.mcilhagga@gmail.com>
2023-10-11 15:35:20 +0100

    changed pyproject

--- tests/example1.py
+++ tests/example1.py
@@ -5,20 +5,19 @@ Created on Fri Oct  6 13:29:29 2023
 @author: willi
 """
 
-import os
-import sys
+#import os
+#import sys
 
 # patch the path to access minawk as if it was installed
-up = os.path.normpath(os.path.join(os.getcwd(), "../src"))
-sys.path.append(up)
-sys.path.append(os.getcwd())
+#up = os.path.normpath(os.path.join(os.getcwd(), "../src"))
+#sys.path.append(up)
+#sys.path.append(os.getcwd())
 
 # actual example here
 
-import re
 from awkish import Awk
 
-myawk = Awk(FS=re.compile(" +"))
+myawk = Awk()
 
 @myawk.beginjob
 def begin(nr):
